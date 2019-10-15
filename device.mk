2ce35a178e556a86d8cac27ec40cdace958f47d6
Patrick Tjin <pattjin@google.com>
2019-10-15 10:32:37 +0800

    raphael: Add nosdcard characteristic to device
    * Just as per stock.
    
    Bug: 30895189
    Change-Id: Ic2e040dc8d811ca84c910d42f6f7db35526802ff

--- device.mk
+++ device.mk
@@ -12,6 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/raphael/raphael-vendor.mk)
 
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
